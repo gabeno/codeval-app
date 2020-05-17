4d007145755ff8e2f678f6b1fb01f3a76f5a6521
Gabriel Majivu <gabriel@angaza.com>
2020-05-17 22:24:45 +0300

    use development config
    also set `FLASK_ENV=development`
    
    ```
    $ python manage.py run
     * Serving Flask app "project/__init__.py" (lazy loading)
     * Environment: development
     * Debug mode: on
     * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
     * Restarting with stat
     * Debugger is active!
     * Debugger PIN: 125-412-714
    ```
    
    Ref: https://flask.palletsprojects.com/en/1.0.x/quickstart/#debug-mode

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify
 
+#instantiate the app
 app = Flask(__name__)
+
+# set config
+app.config.from_object('project.config.DevelopmentConfig')
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
